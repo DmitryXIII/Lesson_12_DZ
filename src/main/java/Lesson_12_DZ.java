ec37c5dc763a6de965bf6298bfdfb1fd56e0f872
Dmitry Gusev <dmtrxiii@gmail.com>
2021-11-01 16:55:37 +0300

    изменена логика работы потоков

--- src/main/java/Lesson_12_DZ.java
+++ src/main/java/Lesson_12_DZ.java
@@ -3,10 +3,7 @@ import java.util.Arrays;
 public class Lesson_12_DZ {
     static final int SIZE = 10000000;
     static final int HALF = SIZE / 2;
-    static float[] halfArray1;
-    static float[] halfArray2;
     static Object lock = new Object();
-    static int COUNTER;
 
     public static void main(String[] args) {
         method1();
@@ -26,24 +23,21 @@ public class Lesson_12_DZ {
 
     public static void method2() {
         float[] arrayFromMethod2 = new float[SIZE];
-
         Arrays.fill(arrayFromMethod2, 1);
 
         long a = System.currentTimeMillis();
 
+        float[] halfArray2 = new float[HALF];
+
+        MyThread threadForHelp = new MyThread(arrayFromMethod2);
+        threadForHelp.start();
+
+        System.arraycopy(arrayFromMethod2, HALF, halfArray2, 0, HALF);
+        for (int i = 0; i < halfArray2.length; i++) {
+            halfArray2[i] = (float) (halfArray2[i] * Math.sin(0.2f + (i + HALF) / 5) * Math.cos(0.2f + (i + HALF) / 5) * Math.cos(0.4f + (i + HALF) / 2));
+        }
         synchronized (lock) {
-            halfArray1 = new float[HALF];
-            halfArray2 = new float[HALF];
-            System.arraycopy(arrayFromMethod2, 0, halfArray1, 0, HALF);
-            System.arraycopy(arrayFromMethod2, HALF, halfArray2, 0, HALF);
-
-            new Thread(new MyThread()).start();
-
-            for (int i = 0; i < halfArray2.length; i++) {
-                halfArray2[i] = (float) (halfArray2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-
-            while (COUNTER < halfArray1.length) {
+            while (!threadForHelp.isItsDone()) {
                 try {
                     lock.wait();
                 } catch (InterruptedException e) {
@@ -51,18 +45,37 @@ public class Lesson_12_DZ {
                 }
             }
         }
-        System.arraycopy(halfArray1, 0, arrayFromMethod2, 0, HALF);
+        System.arraycopy(threadForHelp.getHalfArray1(), 0, arrayFromMethod2, 0, HALF);
         System.arraycopy(halfArray2, 0, arrayFromMethod2, HALF, HALF);
         System.out.println("method2 выполнялся " + (System.currentTimeMillis() - a) + " миллисекунд");
     }
 
+
     static class MyThread extends Thread {
+        private float[] halfArray1;
+        private boolean itsDone;
+
+        public MyThread(float[] arrayFromMethod2) {
+            this.halfArray1 = new float[Lesson_12_DZ.HALF];
+            System.arraycopy(arrayFromMethod2, 0, halfArray1, 0, Lesson_12_DZ.HALF);
+            this.itsDone = false;
+        }
+
+        public boolean isItsDone() {
+            return itsDone;
+        }
+
+        public float[] getHalfArray1() {
+            return halfArray1;
+        }
+
         @Override
         public void run() {
             synchronized (lock) {
-                for (COUNTER = 0; COUNTER < halfArray1.length; COUNTER++) {
-                    halfArray1[COUNTER] = (float) (halfArray1[COUNTER] * Math.sin(0.2f + COUNTER / 5) * Math.cos(0.2f + COUNTER / 5) * Math.cos(0.4f + COUNTER / 2));
+                for (int i = 0; i < this.halfArray1.length; i++) {
+                    halfArray1[i] = (float) (halfArray1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
+                this.itsDone = true;
                 lock.notify();
             }
         }
